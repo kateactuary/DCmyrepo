902d881349168e936e95ca63037f2d3dea007e66
KC <luyingchen2008@gmail.com>
2021-11-28 20:56:13 +0800

    updated comments

--- Date_Time001.R
+++ Date_Time001.R
@@ -3,3 +3,6 @@ today()
 my_day <- mdy("Sept 10, 1986")
 #calculate date difference
 difftime(today(), my_day, units = "days")
+#now function tells time now
+now()
+#units are "secs", "mins", "days", "weeks", "hours"
